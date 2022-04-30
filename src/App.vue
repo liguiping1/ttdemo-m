d46eae1d362804c4e771d2203c66c6815bc23399
liguiping <1532889875@qq.com>
2022-05-01 04:19:26 +0800

    项目初始化-跳转初始目录

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <h1>黑马头条</h1>
     <router-view/>
   </div>
 </template>
